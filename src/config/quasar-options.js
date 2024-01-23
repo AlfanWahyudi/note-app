5c5cbe3d3d99f157eb5d80898d49ccdf13393da3
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2024-01-23 15:20:59 +0700

    update: quasar color config

---
+++ src/config/quasar-options.js
@@ -0,0 +1,15 @@
+export default {
+    plugins: {},
+    config: {
+        brand: {
+            primary: '#40E0D0',
+            secondary: '#B9F8D3',
+            accent: '#E78EA9',
+            dark: '#1B1B1B',
+            positive: '#66FF00',
+            negative: '#FF0800',
+            info: '#89CFF0',
+            warning: '#ffd800',
+        }
+    }
+}
